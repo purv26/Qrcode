c00e683a32a39716aa1b8f460816000037da1a48
purva <purvasalvi7@gmail.com>
2022-02-02 14:44:43 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View ,Button,Image,TextInput} from 'react-native';
+class App extends React.Component {
+    constructor()
+    {
+        super();
+        this.state={
+            Name:'',
+            Address:'',
+            Email:'',
+            MobileNumber:'',
+        }
+    }
+    submit()
+    {
+        console.warn(this.state);
+    }
+    render(){
+        return(
+        <View  style={{margin:20 ,marginTop:50}}>
+        <TextInput maxLength={15}
+        placeholder= "Enter name" 
+        onChangeText={(text) => {this.setState({name: text })}}
+        style={{borderWidth:4,borderColor: 'skyblue',margin:20}}
+        />
+        <TextInput maxLength={200}
+         placeholder="Enter Your Address" 
+        onChangeText={(text) => {this.setState({name: text })}}
+        style={{borderWidth:4,borderColor: 'skyblue',margin:20}}
+        />
+        <TextInput
+         placeholder="Enter Your Email" 
+        onChangeText={(text) => {this.setState({name: text })}}
+        style={{borderWidth:4,borderColor: 'skyblue',margin:20}}
+        />
+         <TextInput  KeyboardType={'numeric'}
+          placeholder="Enter Your MobileNumber " 
+        onChangeText={(text) => {this.setState({name: text })}}
+        style={{borderWidth:3,borderColor: 'skyblue',margin:20}}
+        />
+        <Button title="Make Payment" onPress={() => {this.submit()}}/>
+        <Image source={require('C:/Users/hi/codeqr/assets/PhonePeQR_7798130209.png')}/>
+        
+        </View>
+        )
+        }
 }
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
